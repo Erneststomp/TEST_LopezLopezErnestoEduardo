c27f3391c993372286f9f7109e7cc9277713287b
ErnestStomp <ernesto.lopez.lbm@outlook.com>
2023-02-24 16:23:08 -0600

    correciones

--- src/App.js
+++ src/App.js
@@ -7,34 +7,45 @@ import './App.css'
 function App() {
   const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const [activator, setActivator] = useState(false);
 
   const handleSearch = (searchResults) => {
     setMovies(searchResults);
     setSelectedMovie(null);
+    setActivator(searchResults.length > 0);
   };
 
   const handleMovieClick = (movie) => {
     setSelectedMovie(movie);
   };
 
-
   return (
     <div>
-    <h1 className="Titulo">Ernesto Lopez Search IMDb</h1>
-    <SearchBox onSearch={handleSearch} />
-    {selectedMovie ? (
-      <MovieDetails movie={selectedMovie}  />
-    ) : (
-      <>
-        {movies.length > 0 ? (
-          <MovieList movies={movies} onMovieClick={handleMovieClick} />
-        ) : (
-          <p>Enter a search term to see results</p>
-        )}
-      </>
-    )}
-  </div>
-  )  
+      <div className="TitleDiv"><h1 className="Titulo">Ernesto Lopez Search IMDb</h1></div>
+      
+      <div
+        className={activator ? "activator-on animate" : "activator-off"}
+        onAnimationEnd={() => {
+          if (!activator) {
+            setMovies([]);
+          }
+        }}
+      >
+        <SearchBox onSearch={handleSearch} />
+      </div>
+      {selectedMovie ? (
+        <MovieDetails movie={selectedMovie} />
+      ) : (
+        <>
+          {movies.length > 0 ? (
+            <MovieList movies={movies} onMovieClick={handleMovieClick} />
+          ) : (
+            <p>Enter a search term to see results</p>
+          )}
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -1,24 +1,46 @@
 import React, { useState, useEffect } from "react";
 import './MovieDetails.css'
 
-function MovieDetail(props) {
+function MovieDetails(props) {
   const [movieDetails, setMovieDetails] = useState(null);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
   useEffect(() => {
-    fetch(`http://localhost/detailedsearch.php?selectedMovie=${props.movie.id}`)
-      .then((response) => {return response.json()})
-      .then((datadetailed) => {
+    const fetchMovieDetails = async () => {
+      try {
+        const response = await fetch(`http://localhost/detailedsearch.php?selectedMovie=${props.movie.id}`);
+        const datadetailed = await response.json();
         setMovieDetails(JSON.parse(datadetailed));
-        console.log(JSON.parse(datadetailed));
-      })
-      .catch((error) => console.log(error));
+        console.log('2');
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchMovieDetails();
   }, [props.movie.id]);
+
+  const handleImageChange = (index) => {
+    setCurrentImageIndex(index);
+  }
 
   return (
     <div>
       <div className="movie">
         <div className="movie-poster">
-          <img className="DetailPoster" src={props.movie.image} alt={props.movie.title} />
+          {movieDetails && movieDetails.images.items && movieDetails.images.items.length > 0 && (
+            <>
+              <img className="DetailPoster" src={props.movie.image} alt={props.movie.title} />
+              {movieDetails.images.items.slice(0, 5).map((image, index) => (
+              <img
+                key={index}
+                className={`gallery-image ${index === currentImageIndex ? "active" : ""}`}
+                src={image.image}
+                alt={`${props.movie.title} imagen ${index}`}
+                onClick={() => handleImageChange(index)}
+              />
+            ))}
+            </>
+          )}
         </div>
         <div className="movie-details">
           <h2 className="movie-title">{props.movie.title}</h2>
@@ -28,8 +50,8 @@ function MovieDetail(props) {
               <p>Duration: {movieDetails.runtimeMins} mins</p>
               <p>IMDb Rating: {movieDetails.imDbRating}</p>
               <p>Plot: {movieDetails.plot}</p>
-              <p>Generes: {movieDetails.genres}</p>
-              <p>Clasification: {movieDetails.contentRating}</p>
+              <p>Genres: {movieDetails.genres}</p>
+              <p>Classification: {movieDetails.contentRating}</p>
             </>
           ) : (
             <p>Loading movie details...</p>
@@ -40,4 +62,4 @@ function MovieDetail(props) {
   );
 }
 
-export default MovieDetail;
+export default MovieDetails;
